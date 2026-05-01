7cb1c11be267f677fa366b8a2a7b4f020267a664
IsuruMadhushan0430 <155760402+IsuruMadhushan0430@users.noreply.github.com>
2026-05-01 11:25:29 +0530

    model developing process

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='medical_chatbot',
     version='0.1.0',
     author='Isuru Madhushan',
-    author_email='isurumadhushan0430@example.com',
+    author_email='isurumadhushan0430@gmail.com',
     packages=find_packages(),
     install_requires=[],
 )
